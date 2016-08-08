f1a4987ba212c9ed81d48c3bbcb7b6a9146f3145
TE-ToshiakiTanaka <Toshiaki.Tanaka@jp.sony.com>
2016-08-08 20:37:26 +0900

    update.

--- project/fleet/script/testcase_normal.py
+++ project/fleet/script/testcase_normal.py
@@ -110,7 +110,9 @@ class TestCase(testcase.TestCase_Base):
             time.sleep(10)
         while self.tap_timeout("next.png", loop=3, timeout=2): time.sleep(5)
         while not self.enable_timeout("attack_withdrawal.png", loop=3, timeout=2):
-            self.tap_timeout("return.png", loop=3, timeout=2); time.sleep(5)
+            if self.enable_timeout("return.png", loop=3, timeout=1):
+                self.adb_screenshot("drop_%s.png" % self.adb.get().SERIAL)
+                self.tap_timeout("return.png", loop=3, timeout=2)
         self.tap_timeout("attack_withdrawal.png"); time.sleep(1)
         return self.enable_timeout("home.png")
 
